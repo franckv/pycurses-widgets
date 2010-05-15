fdc0bc49e8fd3f238ff25d822c42c359a9c7842b
franckv <franck.vdl@gmail.com>
2010-05-15 14:18:37 +0200

    reorganize imports

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
 from screen import Screen
+from statusbar import StatusBar
+from commandbar import CommandBar
+from titlebar import TitleBar
+from panel import Panel
+from textpanel import TextPanel
+from itemlist import ItemList
+
